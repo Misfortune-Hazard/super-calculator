79a51c6a426668dcc54a7ba61a3ae1b23457f506
Kalel Molano <contagiousmisfortune@gmail.com>
2024-09-13 06:36:22 -0500

    Sistema de suma arreglado

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ while playing:
     user_input = int(input())
 
     if user_input == 1:
-        print("La suma es...")
+        print("La suma es...", num1 + num2)
     elif user_input == 2:
         print("La resta es...")
